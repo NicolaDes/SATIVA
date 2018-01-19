e6659fac3b864b1cfebafd1c7935460d88b7411b
NicolaDes <sphero@outlook.it>
2018-01-19 11:21:30 +0100

    Pigeonhole problem added!

---
+++ lib/Pigeonhole.hh
@@ -0,0 +1,40 @@
+#ifndef PIGEONHOLE_HH
+#define PIGEONHOLE_HH
+#include "Datastructure.hh"
+#include <vector>
+
+namespace pigeonhole{
+
+void fillWithPigeonhole(Solver& solver, int holes){
+	int n=holes;  // n+1 pigeons in n holes
+	
+	solver.init((n+1)*n, (n+1)+n*(n*(n+1)/2));
+	std::vector<Literal> tmp;
+
+	int i, k;  // pigeons i, k
+	int j;     // hole j
+
+  // n+1 clauses which say that a pigeon has to be placed in some hole
+	for (i=1; i <= n+1; i++) {
+		for (j=1; j <= n; j++)
+			tmp.push_back(Literal(n*(i-1)+j));
+		solver.newClause(tmp);
+		tmp.clear();
+	}
+
+  // for each hole we have a set of clauses ensuring that only one single
+  // pigeon is placed into that hole
+	for (j=1; j <= n; j++){
+		for (i=1; i <= n; i++){
+			for (k=i+1; k <= n+1; k++){
+				tmp.push_back(Literal(-(n*(i-1)+j)));tmp.push_back(-(n*(k-1)+j));
+				solver.newClause(tmp, false);
+				tmp.clear();
+			}
+		}
+	}
+
+};
+}
+
+#endif
---
+++ test/PIG.cc
@@ -0,0 +1,34 @@
+#include "Config.h"
+#include "Datastructure.hh"
+#include "Solver.hh"
+#include "Reader.hh"
+#include "Pigeonhole.hh"
+#include "CommandLineInterface.hh"
+#include <iostream>
+#include <time.h>
+
+typedef std::vector<Literal> LiteralList;
+typedef std::vector<Clause> ClauseList;
+
+int main(int argc, char** argv){
+	
+
+	Solver solver;
+
+
+	int holes = atoi(argv[1]);
+
+	pigeonhole::fillWithPigeonhole(solver, holes);
+
+	tabular::printInit(&solver);
+	tabular::printLaunch(&solver);
+	bool res = solver.CDCL();
+	if(res) tabular::printModel(&solver);
+	else{
+		tabular::generateGV(&solver);
+	}
+
+
+
+	return 0;
+}
