6908b0cb19170d5d4f49fb809a94805b95a81bfa
NicolaDes <sphero@outlook.it>
2018-02-08 11:33:39 +0100

    Fixed generate_doc.sh

--- generate_doc.sh
+++ generate_doc.sh
@@ -4,7 +4,7 @@ fi
 echo "Generating doxygen in docs..."
 doxygen Doxygen.cfg 
 echo "Ended generation!"
-if ! type firefoxs > /dev/null; then
+if ! type firefox > /dev/null; then
 	echo  -e "\e[31mFirefox not installed! open < docs/html/index.html > with your browser\e[39m"
 else
 	firefox docs/html/index.html
